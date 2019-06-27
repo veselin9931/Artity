3f39e590a04b0e666e4cbc6b7a1ae0f7a2b10a44
Veselin Georgiev <ves.georgiev99@gmail.com>
2019-06-27 14:31:46 +0300

    Offert.cs,Rating.cs added

---
+++ Data/Artity.Data.Models/Offert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Artity.Data.Models
+{
+    public class Offert
+    {
+        public string Id { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime Еngagement { get; set; }
+
+        public string Features { get; set; }
+
+        public string Message { get; set; }
+
+        public OfertType Type { get; set; }
+
+    }
+}
