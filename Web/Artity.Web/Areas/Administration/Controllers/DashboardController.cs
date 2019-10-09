abd8ea67eb1439cca88115e49358d7c7f96b7b14
v.georgiev9931 <ves.georgiev99@gmail.com>
2019-10-09 13:59:05 +0300

    I cleared a little of the template.

--- Web/Artity.Web/Areas/Administration/Controllers/DashboardController.cs
+++ Web/Artity.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -1,23 +1,10 @@
 ﻿namespace Artity.Web.Areas.Administration.Controllers
 {
-    using Artity.Services.Data;
     using Artity.Web.Areas.Administration.ViewModels.Dashboard;
 
     using Microsoft.AspNetCore.Mvc;
 
     public class DashboardController : AdministrationController
     {
-        private readonly ISettingsService settingsService;
-
-        public DashboardController(ISettingsService settingsService)
-        {
-            this.settingsService = settingsService;
-        }
-
-        public IActionResult Index()
-        {
-            var viewModel = new IndexViewModel { SettingsCount = this.settingsService.GetCount(), };
-            return this.View(viewModel);
-        }
     }
 }
