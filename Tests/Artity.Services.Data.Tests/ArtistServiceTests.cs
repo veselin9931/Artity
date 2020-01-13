fc0348787bfc264e2be1aaf7c68ad9b1835e1e86
v.georgiev9931 <ves.georgiev99@gmail.com>
2020-01-13 21:07:28 +0200

    Add new tests.

---
+++ Tests/Artity.Services.Data.Tests/ArtistServiceTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Artity.Services.Data.Tests
+{
+    using Artity.Data.Common.Repositories;
+    using Artity.Data.Models;
+    using Artity.Web.ViewModels;
+    using Microsoft.EntityFrameworkCore;
+    using Moq;
+    using NUnit.Framework;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ArtistServiceTests
+    {
+        private IArtistService artistService;
+
+        [SetUp]
+        public void Init()
+        {
+            var mockArtistRepository = new Mock<IDeletableEntityRepository<Artist>>();
+
+            var artist = new List<Artist>
+            {
+                new Artist() { Nikname = "aaa"},
+                new Artist() { Nikname = "bbb"},
+                new Artist() { Nikname = "ccc"},
+            };
+
+            mockArtistRepository.Setup(mr => mr.All()).Returns(artist.AsQueryable());
+
+            this.artistService = new ArtistService(mockArtistRepository.Object);
+        }
+
+        [Test]
+        public void GetAllArtistsShouldGetAllNotDeletedArtistsInRepository()
+        {
+            var artist = new List<Artist>
+            {
+                new Artist() { Nikname = "aaa"},
+                new Artist() { Nikname = "bbb"},
+                new Artist() { Nikname = "ccc"},
+            };
+
+            this.artistService.GetAllArtists<AllArtistViewModel>();
+
+        }
+    }
+}
