388eea59f16553242c5ead92fcb1ddb3c09c6db1
v.georgiev9931 <ves.georgiev99@gmail.com>
2020-01-17 22:09:44 +0200

    Add ServiceModels.

---
+++ Services/Artity.Services.Data.ServiceModels/PictureServiceModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Artity.Services.Data.ServiceModels
+{
+    using Artity.Data.Models;
+    using Artity.Services.Mapping;
+
+    public class PictureServiceModel : IMapFrom<Picture>, IMapTo<Picture>, IMapTo<PictureServiceModel>
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Link { get; set; }
+    }
+}
---
+++ Tests/Artity.Services.Data.Tests/BaseServiceTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Artity.Tests.ServicesTests
+{
+    using System.Reflection;
+    using Artity.Data.Models;
+    using Artity.Services.Data.ServiceModels;
+    using Artity.Services.Data.Tests.TestViewModels;
+    using Artity.Services.Mapping;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public abstract class BaseServiceTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            this.RegisterMappings();
+        }
+
+        private void RegisterMappings()
+        {
+            AutoMapperConfig.RegisterMappings(typeof(Picture).GetTypeInfo().Assembly, typeof(PictureServiceModel).GetTypeInfo().Assembly, typeof(PictureTestViewModel).GetTypeInfo().Assembly);
+        }
+    }
+}
