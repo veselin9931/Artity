0ed888044e95ce305cb5f748d0d337c9534e6e41
v.georgiev9931 <ves.georgiev99@gmail.com>
2020-01-18 15:52:57 +0200

    Add new lib for TestViewModels. Add clean some references.

---
+++ Services/Artity.Services.Data.ServiceModels/ArtistServiceModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Artity.Services.Data.ServiceModels
+{
+    using Artity.Data.Models;
+    using Artity.Services.Mapping;
+
+    public class ArtistServiceModel : IMapFrom<Artist>
+    {
+        public string Nikname { get; set; }
+    }
+}
---
+++ TestViewModels/GetArtistsTestViewModels.cs
@@ -0,0 +1,27 @@
+﻿ namespace TestViewModels
+{ 
+    using Artity.Services.Data.ServiceModels;
+    using Artity.Services.Mapping;
+    using System;
+
+    public class GetArtistsTestViewModels : IMapFrom<ArtistServiceModel>, IComparable
+    {
+        public string Nikname { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            var myprops = typeof(GetArtistsTestViewModels).GetProperties().Length;
+
+            var objProp = obj.GetType().GetProperties().Length;
+
+            if(myprops != objProp)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+
+ 
+}
