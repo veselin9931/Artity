47f4dc86a057685f55e54e534c994a58bdacf153
v.georgiev9931 <ves.georgiev99@gmail.com>
2020-01-19 17:38:58 +0200

    Add static ExceptionMessages.

---
+++ Artity.Common/ExeptionMessages/ArtistExeptionMesseges.cs
@@ -0,0 +1,9 @@
+﻿namespace Artity.Common.ExeptionMessages
+{
+    public static class ArtistExeptionMesseges
+    {
+        public const string NewArtist_InvalidParams = "Invalid parameters {0}";
+
+        public const string GetArtist_ZeroArtistInRepo = "Invalid parameters {0}";
+    }
+}
